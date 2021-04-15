c1897754c994bdeae5ccdd005136b3430010ad18
Mohamed Ghazi <m.mahmoud@uplift.plus>
2021-04-15 17:42:20 +0200

    Update departments styles and structure

--- src/Layout/component/OurDeprtments.js
+++ src/Layout/component/OurDeprtments.js
@@ -4,6 +4,7 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 
 // style
 import './style/OurDeprtments.scss';
+import DepartmentBox from "./Block/DepartmentBox";
 
 export default function OurDepartments()
 {
@@ -18,15 +19,24 @@ export default function OurDepartments()
 
     const {galleryItems, currentIndex} = state;
 
+    const departments = [
+        {name: "Medicine", about: "Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced", icon: "flaticon-pills icon-med"},
+        {name: "Cardeology", about: "Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced", icon: "flaticon-medical-5 icon-med"},
+        {name: "Dental Care", about: "Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced", icon: "flaticon-medical icon-med"},
+        {name: "Medicine", about: "Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced", icon: "flaticon-pills icon-med"},
+        {name: "Cardeology", about: "Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced", icon: "flaticon-human-hip icon-med"},
+        {name: "Dental Care", about: "Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced", icon: "flaticon-medical icon-med"}
+    ]
+
     return(
         <div id="our-departments">
             <div className="container">
-                <div className="content">
+                <div className="departments-header">
                     <h5 className="title">Our Departments</h5>
                     <span className="sub-title">Dedicated Services</span>
                     <div className="line"> </div>
                 </div>
-                <div className="alice-carousel">
+                <div className="departments-content">
                     <AliceCarousel
                         items={galleryItems}
                         responsive={responsive}
@@ -38,54 +48,12 @@ export default function OurDepartments()
                         mouseTrackingEnabled={true}
                         slideToIndex={currentIndex}
                     >
-                        <div className="alice-content">
-                            <i className="flaticon-medical icon-med"> </i>
-                            <span className="number">01.</span>
-                            <h5 className="title">Dental Care</h5>
-                            <p className="description">Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced</p>
-                            <button className="read-btn">Read More</button>
-                            <i className="fas fa-long-arrow-alt-right"> </i>
-                        </div>
-                        <div className="alice-content">
-                            <i className="flaticon-pills icon-med"> </i>
-                            <span className="number">02.</span>
-                            <h5 className="title">Medicine</h5>
-                            <p className="description">Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced</p>
-                            <button className="read-btn">Read More</button>
-                            <i className="fas fa-long-arrow-alt-right"> </i>
-                        </div>
-                        <div className="alice-content">
-                            <i className="flaticon-medical-5 icon-med"> </i>
-                            <span className="number">03.</span>
-                            <h5 className="title">Cardeology</h5>
-                            <p className="description">Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced</p>
-                            <button className="read-btn">Read More</button>
-                            <i className="fas fa-long-arrow-alt-right"> </i>
-                        </div>
-                        <div className="alice-content">
-                            <i className="flaticon-human-hip icon-med"> </i>
-                            <span className="number">04.</span>
-                            <h5 className="title">Orthopedic</h5>
-                            <p className="description">Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced</p>
-                            <button className="read-btn">Read More</button>
-                            <i className="fas fa-long-arrow-alt-right"> </i>
-                        </div>
-                        <div className="alice-content">
-                            <i className="flaticon-medical icon-med"> </i>
-                            <span className="number">05.</span>
-                            <h5 className="title">Dental Care</h5>
-                            <p className="description">Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced</p>
-                            <button className="read-btn">Read More</button>
-                            <i className="fas fa-long-arrow-alt-right"> </i>
-                        </div>
-                        <div className="alice-content">
-                            <i className="flaticon-pills icon-med"> </i>
-                            <span className="number">06.</span>
-                            <h5 className="title">Medicine</h5>
-                            <p className="description">Aorem Ipsumea dummy texte printing setting detry bringin eight challenges faced</p>
-                            <button className="read-btn">Read More</button>
-                            <i className="fas fa-long-arrow-alt-right"> </i>
-                        </div>
+                        {departments?.map((department, i) => {
+                            return <div key={i}>
+                                <DepartmentBox department={i + 1} name={department.name} about={department.about} icon={department.icon} />
+                            </div>
+                        })}
+
                     </AliceCarousel>
                 </div>
             </div>
