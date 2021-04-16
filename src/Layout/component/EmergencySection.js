156414878a2aa3da7885aa187802e835641d6c67
maryam-algendy <maryamalgendy@gmail.com>
2021-04-16 05:12:21 +0200

    add benefits , testimonial, emergency and blogs sections

--- src/Layout/component/EmergencySection.js
+++ src/Layout/component/EmergencySection.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-export  default function EmergencySection()
-{
-    return <div id="emergency">
-        Emergency
-    </div>
+//style
+import "./style/EmergencySection.scss";
+
+export default function EmergencySection() {
+    return (
+        <div id="emergency">
+            <div className="inner-text">
+                <h3>for emergency cases</h3>
+                <span>1-800-555-0120</span>
+                <p>
+                    Building a healthy environment that supports development for the community.
+                    Your personal case manager will ensure that you receive the best possible care.
+                </p>
+            </div>
+        </div>
+    )
 }
--- src/Layout/component/Testimonial.js
+++ src/Layout/component/Testimonial.js
@@ -1,8 +1,46 @@
 import React from "react";
+import {Button} from "react-bootstrap";
 
-export  default function Testimonial()
-{
-    return <div id="testimonial">
-        Testimonial
-    </div>
+//style
+import "./style/Testimonial.scss";
+
+export default function Testimonial() {
+    return (
+        <div id="testimonial">
+            <div className="overlay">
+                <div className="inner-text">
+                    <h3>testimonials</h3>
+                    <div className="nav-btns">
+                        <Button>
+                            <i className="fas fa-angle-left"></i>
+                        </Button>
+                        <Button>
+                            <i className="fas fa-angle-right"></i>
+                        </Button>
+                    </div>
+                    {
+                        [1, 2].map((item, id) => {
+                                return (
+                                    <div key={id} className="item">
+                                        <p>
+                                            josef ardogon
+                                            <span> / CEO Artland</span>
+                                        </p>
+                                        <span className="rate">
+                                            <i className="fas fa-star"></i>
+                                            <i className="fas fa-star"></i>
+                                            <i className="fas fa-star"></i>
+                                            <i className="fas fa-star"></i>
+                                            <i className="fas fa-star"></i>
+                                        </span>
+                                        <i>"Eodem modo typi, qui nunc nobis videntur parum clar fiant sollemnes in futurum.
+                                            Lorem ipsum dolor sit amet tetuer adipiscing elit, sed diam nonu."</i>
+                                    </div>
+                                )
+                            }
+                        )}
+                </div>
+            </div>
+        </div>
+    )
 }
