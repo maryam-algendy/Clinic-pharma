66ff5b376744d1642cfb7b84f5cfcfcd70ac4024
Mohamed Ghazi <m.mahmoud@uplift.plus>
2021-05-24 09:37:13 +0200

    Prevent contextmenu and inspector on PRODUCTION mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react";
-import {BrowserRouter} from "react-router-dom";
-import {Switch, Route} from "react-router";
-import {useDispatch} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router";
+import { useDispatch } from "react-redux";
 
 // pages
 import Home from "./Layout/Home";
@@ -22,12 +22,12 @@ import OnlinePayment from "./Layout/OnlinePayment";
 import storage from "./utilize/storage";
 import NewBlog from "./Layout/NewBlog";
 // actions
-import {loadBlogs, loadCart, loadSlides, loadTopDoctors} from "./actions";
+import { loadBlogs, loadCart, loadSlides, loadTopDoctors } from "./actions";
 import SingleBlog from "./Layout/SingleBlog";
 import Chat from "./Layout/Chat";
 import MedicalHistory from "./Layout/MedicalHistory";
 
-export default function App() {
+export default function App () {
     const dispatch = useDispatch();
     const [auth, setAuth] = useState(true);
 
@@ -41,7 +41,39 @@ export default function App() {
         if (storage("access-token")) {
             setAuth(storage("user")?.tokens[0]?.token === storage("access-token"));
         }
-    }, [dispatch])
+
+        /**
+         * If it's production mode, prevent inspector!
+         */
+        if (process.env.REACT_APP_MODE !== "DEV") {
+            // prevent contextmenu
+            document.addEventListener('contextmenu', event => event.preventDefault());
+
+            // prevent open inspector
+            document.onkeydown = function (e) {
+                if (e.keyCode === 123) {
+                    console.log('Your presence here for any reason will put you on the blacklist, and action will be taken with you, but your data is also vulnerable.');
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
+                    console.log('Your presence here for any reason will put you on the blacklist, and action will be taken with you, but your data is also vulnerable.');
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
+                    console.log('Your presence here for any reason will put you on the blacklist, and action will be taken with you, but your data is also vulnerable.');
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
+                    console.log('Your presence here for any reason will put you on the blacklist, and action will be taken with you, but your data is also vulnerable.');
+                    return false;
+                }
+                if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
+                    console.log('Your presence here for any reason will put you on the blacklist, and action will be taken with you, but your data is also vulnerable.');
+                    return false;
+                }
+            }
+        }
+    }, [dispatch]);
 
     return (
         <div id="clinic-pharma">
