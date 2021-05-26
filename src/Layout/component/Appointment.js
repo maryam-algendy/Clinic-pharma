ad62fe3f9c67431047e81746d6a963b776fbd28d
Mohamed Ghazi <m.mahmoud@uplift.plus>
2021-05-27 01:01:24 +0200

    Solve cancel appointment issue

--- src/Layout/component/Appointment.js
+++ src/Layout/component/Appointment.js
@@ -6,32 +6,31 @@ import './style/Appointment.scss';
 import API from "../../utilize/API";
 import storage from "../../utilize/storage";
 import dateConverter from "../../utilize/dateConverter";
-import { Link } from "react-router-dom";
 
-export default function Appointment()
-{
+export default function Appointment () {
     const [appointments, setAppointments] = useState([]);
     const [form, setForm] = useState({ time: "", date: "" });
     const [show, setShow] = useState(false);
     const [error, setError] = useState();
-    const [param,setParam]=useState();
-    const handleClose = () => setShow(false);
+    const [action, setAction] = useState();
+    const [deletedAppointmentAssets, setDeletedAppointmentAssets] = useState({});
 
-    function deleteAppointment(id) {
-        API(`appointment/cancel/?appointment=${id}`, "DELETE", form)
+    function deleteAppointment (id) {
+        API(`appointment/cancel/?appointment=${id}`, "DELETE")
             .then(({ data, status }) => {
                 if (status === 200) {
-                    fetchAppointments();
                     setError("Deleted Successfully");
                     setTimeout(() => {
                         setShow(false);
-                    }, 1500)
+                    }, 500);
+                    fetchAppointments();
                 } else {
                     setError(data?.message);
                 }
             })
     }
-    function addAppointment() {
+
+    function addAppointment () {
         API("appointment", "POST", form)
             .then(({ data, status }) => {
                 if (status === 200) {
@@ -45,49 +44,54 @@ export default function Appointment()
                 }
             })
     }
-    function fetchAppointments() {
-        API("appointment")
-            .then(({ data, status }) => {
-                if (status === 200) {
-                    setAppointments(data?.appointments);
-                } else {
-                    console.log(data);
-                }
-            })
+
+    function fetchAppointments () {
+        API("appointment").then(({ data, status }) => {
+            if (status === 200) {
+                setAppointments(data?.appointments);
+            } else {
+                console.log(data);
+            }
+        })
     }
 
     useEffect(() => {
-       fetchAppointments();
+        fetchAppointments();
     }, [])
 
 
-    function modal(path,id){
-        return(
-            <Modal show={show} onHide={handleClose} animation={false}>
+    function modal () {
+        return (
+            <Modal show={show} onHide={() => setShow(false)} animation={false}>
                 {
-                    (path === "add") ?
+                    (action === "add") ?
                         <>
-                                {error ? <div className={error === "Added Successfully" ? "alert-primary" : "alert-danger"}>{error}</div> : null}
+                            {error ? <div className={error === "Added Successfully" ? "alert-primary" : "alert-danger"}>{error}</div> : null}
                             <Modal.Header closeButton>
                                 <Modal.Title className="title">Add Appointment</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
-                                <Form.Control value={form.time} onChange={(e) => setForm({ ...form, time: e.target.value })} placeholder="Add Time *" />
-                                <Form.Control value={form.date} onChange={(e) => setForm({ ...form, date: e.target.value })} className="date-btn" placeholder="Add Date *" />
+                                <Form.Control value={form.time}
+                                              onChange={(e) => setForm({ ...form, time: e.target.value })}
+                                              placeholder="Add Time *"/>
+                                <Form.Control value={form.date}
+                                              onChange={(e) => setForm({ ...form, date: e.target.value })}
+                                              className="date-btn" placeholder="Add Date *"/>
                             </Modal.Body>
                             <Modal.Footer>
                                 <Button onClick={() => addAppointment()}>Save</Button>
                             </Modal.Footer>
                         </>
-                    :
+                        :
                         <>
-                            {error ? <div className={error === "Deleted Successfully" ? "alert-primary" : "alert-danger"}>{error}</div> : null}
+                            {error ? <div
+                                className={error === "Deleted Successfully" ? "alert-primary" : "alert-danger"}>{error}</div> : null}
                             <Modal.Header closeButton>
-                                <Modal.Title className="title">are you sure to delete your appointment with dr:{path}</Modal.Title>
+                                {deletedAppointmentAssets?.doctor && <Modal.Title className="title">are you sure to delete your appointment with Dr: {deletedAppointmentAssets?.doctor}</Modal.Title>}
                             </Modal.Header>
                             <Modal.Footer>
                                 <Button onClick={() => setShow(false)}>cancel</Button>
-                                <Button onClick={() => deleteAppointment(id)}>delete</Button>
+                                <Button onClick={() => deleteAppointment(deletedAppointmentAssets?.appointment)}>delete</Button>
                             </Modal.Footer>
                         </>
                 }
@@ -95,18 +99,23 @@ export default function Appointment()
         )
     }
 
-    return(
+    return (
         <div id="appointment">
             <div className="add-appointment">
                 <div className="appointment-btn">
-                    <button  onClick={()=>{
-                        setShow(true);
-                        setParam("add");
-                    }}><i className="fas fa-plus"> </i>Add Appointment</button>
+                    <button onClick={() => {
+                        if (storage("role") === "patient") {
+                            window.location.href = "/appointment/reserve";
+                        } else {
+                            setShow(true);
+                            setAction("add");
+                        }
+                    }}>
+                        <i className="fas fa-plus"> </i> Add Appointment
+                    </button>
                 </div>
-                {
-                    modal(param)
-                }
+
+                {modal(action)}
             </div>
 
             <div className="table">
@@ -125,23 +134,26 @@ export default function Appointment()
                     <tbody>
                     {appointments?.map((appointment, i) => <tr key={i}>
                         <td>#{i + 1}</td>
-                        <td>{storage("role") === "patient" ? <Nav.Link href={`/doctor/${appointment?.doctor?.name}`}>{appointment?.doctor?.name}</Nav.Link> : !appointment.reserved ? "Not Reserved" : appointment?.patient?.name}</td>
+                        <td>{storage("role") === "patient" ? <Nav.Link
+                            href={`/doctor/${appointment?.doctor?.name}`}>{appointment?.doctor?.name}</Nav.Link> : !appointment.reserved ? "Not Reserved" : appointment?.patient?.name}</td>
                         <td>{dateConverter(appointment?.date)}</td>
                         <td>{appointment?.time}</td>
-                        <td>{storage("role") === "patient" ? appointment?.doctor?.specialization : !appointment.reserved ? "-" : <Nav.Link href={`/history/${appointment?.patient?._id}`}><i className='fa fa-notes-medical'> </i></Nav.Link>}</td>
+                        <td>{storage("role") === "patient" ? appointment?.doctor?.specialization : !appointment.reserved ? "-" :
+                            <Nav.Link href={`/history/${appointment?.patient?._id}`}><i
+                                className='fa fa-notes-medical'> </i></Nav.Link>}</td>
                         <td>
                             {!appointment.reserved ? "-" : <Nav.Link href="/chat">
                                 <i className="fa fa-paper-plane message-icon"> </i>
                             </Nav.Link>}
                         </td>
                         <td>
-                            <Button className="delete" onClick={()=> {
+                            <Button className="delete" onClick={() => {
                                 setShow(true);
-                                setParam(appointment?.doctor?.name,appointment?._id);
-                                console.log(appointment._id)
-                            }
-                            }
-                            ><i className="fas fa-times"></i></Button>
+                                setAction("delete");
+                                setDeletedAppointmentAssets({ doctor: appointment?.doctor?.name, appointment: appointment?._id });
+                            }}>
+                                <i className="fas fa-times"> </i>
+                            </Button>
                         </td>
                     </tr>)}
                     </tbody>
